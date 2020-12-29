4cd8d2e29bd9a6d0acd1343330e7547e5ac8d9d3
Alec Gehlot <alecgehlot@gmail.com>
2020-12-29 22:44:32 +0000

    Imported React and created the business component

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const business = {
     imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
@@ -8,4 +10,27 @@ const business = {
   category: 'Italian',
   rating: 4.5,
   reviewCount: 90
+}
+
+class Business extends React.Component {
+render {
+  return <div className="Business">
+  <div className="image-container">
+    <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+  </div>
+  <h2>{business.name}</h2>
+  <div className="Business-information">
+    <div className="Business-address">
+      <p>{business.address}</p>
+      <p>{business.city}</p>
+      <p>{business.state} {business.zipCode}</p>
+    </div>
+    <div className="Business-reviews">
+      <h3>{business.category}</h3>
+      <h3 className="rating">{business.rating} stars}</h3>
+      <p>{business.reviewCount}</p>
+    </div>
+  </div>
+</div>
+}
 }
